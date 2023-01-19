1b4af2d7a2aadb4a1ee853885d39c7a418a9fb57
vito <vitopiegari01@gmail.com>
2023-01-19 17:03:07 +0100

    Add constants

---
+++ lib/constants/assets.dart
@@ -0,0 +1,1 @@
+const String kLogoTransparent = "assets/logo_transparent.png";
---
+++ lib/constants/strings.dart
@@ -0,0 +1,2 @@
+const String kAppDescription =
+    """Il sistema che si vuole realizzare ha come obiettivo quello di fornire al Sistema Sanitario Nazionale (SSN) una piattaforma di telemonitoraggio del diabete mellito, una fra le malattie croniche più diffuse a livello nazionale. Tale sistema rientra tra gli obiettivi di efficientamento dei servizi e riduzione della spesa pubblica del PNRR.""";
