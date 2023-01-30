d0716bc0ae60dccf14069a4f41ca1b8466d584ac
vito <vitopiegari01@gmail.com>
2023-01-30 17:10:05 +0100

    Add new test user model fields

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -4,5 +4,27 @@ class User {
   final String nome;
   final String cognome;
 
-  User(this.nome, this.cognome);
+  final String codiceFiscale;
+  final DateTime dataNascita;
+  final String indirizzo;
+  final String telefono;
+  final String email;
+  final String tipoDiabete;
+  final String comorbilita;
+  final String farmaciAssunti;
+  final int periodoDiMonitoraggio;
+
+  User(
+    this.nome,
+    this.cognome,
+    this.codiceFiscale,
+    this.dataNascita,
+    this.indirizzo,
+    this.telefono,
+    this.email,
+    this.tipoDiabete,
+    this.comorbilita,
+    this.farmaciAssunti,
+    this.periodoDiMonitoraggio,
+  );
 }
