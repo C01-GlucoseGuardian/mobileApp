1e74d3198250f168d038c63baacf5fe104c072b3
vito <vitopiegari01@gmail.com>
2023-01-30 12:55:05 +0100

    Add feedback questions strings

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -1,2 +1,8 @@
 const String kAppDescription =
     """Il sistema che si vuole realizzare ha come obiettivo quello di fornire al Sistema Sanitario Nazionale (SSN) una piattaforma di telemonitoraggio del diabete mellito, una fra le malattie croniche più diffuse a livello nazionale. Tale sistema rientra tra gli obiettivi di efficientamento dei servizi e riduzione della spesa pubblica del PNRR.""";
+final List<String> kFeedbackSendStrings = [
+  "Come ti sei sentito negli ultimi 10 giorni?",
+  "Quante ore hai dormito negli ultimi 10 giorni?",
+  "Hai avuto fastidi o dolori particolari negli ultimi 10 giorni?",
+  "Sei svenuto o sei stato sul punto di svenire negli ultimi 10 giorni?",
+];
