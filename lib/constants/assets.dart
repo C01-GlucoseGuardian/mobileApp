1b234dfa0fa50ae3c31a04527a42ff753f64c9de
vito <vitopiegari01@gmail.com>
2023-01-31 12:51:36 +0100

    Add landing logic, project description & creators

--- lib/constants/assets.dart
+++ lib/constants/assets.dart
@@ -2,3 +2,5 @@ const String kLogoTransparent = "assets/logo_transparent.png";
 const String kCgmIcon = "assets/cgm_icon.svg";
 const String kPrescriptionsIcon = "assets/prescriptions_icon.svg";
 const String kSyringeIcon = "assets/syringe_icon.svg";
+const String kUndrawProfessor = "assets/undraw_professor.svg";
+const String kUndrawStudent = "assets/undraw_student.svg";
