f735817c8884e30aa49c22199c009538501f6c3e
vito <vitopiegari01@gmail.com>
2023-01-31 12:51:36 +0100

    Add landing logic, project description & creators

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -1,8 +1,13 @@
 const String kAppDescription =
-    """Il sistema che si vuole realizzare ha come obiettivo quello di fornire al Sistema Sanitario Nazionale (SSN) una piattaforma di telemonitoraggio del diabete mellito, una fra le malattie croniche più diffuse a livello nazionale. Tale sistema rientra tra gli obiettivi di efficientamento dei servizi e riduzione della spesa pubblica del PNRR.""";
+    """Il sistema realizzato ha come obiettivo quello di fornire al Sistema Sanitario Nazionale (SSN) una piattaforma di telemonitoraggio del diabete mellito, una fra le malattie croniche più diffuse a livello nazionale. Tale sistema rientra tra gli obiettivi di efficientamento dei servizi e riduzione della spesa pubblica del PNRR.""";
 final List<String> kFeedbackSendStrings = [
   "Come ti sei sentito negli ultimi 10 giorni?",
   "Quante ore hai dormito negli ultimi 10 giorni?",
   "Hai avuto fastidi o dolori particolari negli ultimi 10 giorni?",
   "Sei svenuto o sei stato sul punto di svenire negli ultimi 10 giorni?",
 ];
+
+const String kAppCreators =
+    "Progetto realizzato dagli studenti:\nAldi Matteo,\nCifelli Domenico,\nMarchese Marco Maria,\nMennillo Andrea,\nPiegari Vito official";
+const String kAppUniLectureDesc =
+    "Progetto realizzato per il corso di Ingegneria del Software nel corso di Laurea Triennale in Informatica presso l'Università di Salerno, tenuto dalla Prof.ssa Ferrucci e il Prof. Palomba.";
