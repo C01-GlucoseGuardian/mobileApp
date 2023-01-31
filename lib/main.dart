0ff5b2659fe96c0663f63e1460bd3c120208d50a
vito <vitopiegari01@gmail.com>
2023-01-31 11:25:11 +0100

    Add sharedPreferences helper

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:glucose_guardian/_mock_data.dart';
 import 'package:glucose_guardian/constants/colors.dart';
 import 'package:glucose_guardian/screens/paziente_home.dart';
+import 'package:glucose_guardian/services/shared_preferences_service.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await SharedPreferenceService.init();
+
   initializeDateFormatting('it', null).then((_) => runApp(const MainApp()));
 }
 
