267803ed1ff154733a9022b25ab40bdbde4696d2
vito <vitopiegari01@gmail.com>
2023-02-06 11:17:43 +0100

    Add EmptyData widget component

---
+++ lib/components/empty_data.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:glucose_guardian/constants/assets.dart';
+
+class EmptyData extends StatelessWidget {
+  final String text;
+
+  const EmptyData({super.key, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        SvgPicture.asset(
+          kUndrawErrEng,
+          width: 400,
+        ),
+        const SizedBox(height: 20),
+        Text(
+          text,
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+          ),
+        )
+      ],
+    );
+  }
+}
