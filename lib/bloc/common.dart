71cfb5904c31c1c68935686f4712646cc472f4f5
vito <vitopiegari01@gmail.com>
2023-02-06 10:44:47 +0100

    Depend on bloc

---
+++ lib/bloc/common.dart
@@ -0,0 +1,4 @@
+import 'package:glucose_guardian/services/api_provider.dart';
+import 'package:glucose_guardian/services/api_repository.dart';
+
+final ApiRepository api = ApiRepository(ApiProvider());
