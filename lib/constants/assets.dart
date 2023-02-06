209f942b59cf009b2108d74b862367e3159f5a8f
vito <vitopiegari01@gmail.com>
2023-02-06 11:12:00 +0100

    Add asset

--- lib/constants/assets.dart
+++ lib/constants/assets.dart
@@ -4,3 +4,4 @@ const String kPrescriptionsIcon = "assets/prescriptions_icon.svg";
 const String kSyringeIcon = "assets/syringe_icon.svg";
 const String kUndrawProfessor = "assets/undraw_professor.svg";
 const String kUndrawStudent = "assets/undraw_student.svg";
+const String kUndrawErrEng = "assets/undraw_err_eng.svg";
