3cfbb568e210ae9b000e21067527b9a6f33fff97
vito <vitopiegari01@gmail.com>
2023-02-08 12:56:35 +0100

    Change blue color

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
 const Color kOrange = Color(0xffF8F2ED);
-const Color kBlue = Color(0xffA5D9D9);
+const Color kBlue = Color(0xff3E93EF);
 const Color kOrangePrimary = Color(0xffEF9A3E);
 const Color kBackgroundColor = Color(0xfff9f8fd);
